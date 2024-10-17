cbf3a71becab1029504b91b15c413b103fc1ae2c
Vera Islamova <islamova.vera.mail@yandex.ru>
2024-10-17 17:23:52 +0300

    form with bootsratp ready. going to work with DB

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class BirthdayForm(forms.Form):
+    first_name = forms.CharField(label='Имя', max_length=20)
+    last_name = forms.CharField(
+        label='Фамилия', required=False, help_text='Необязательное поле'
+    )
+    birthday = forms.DateField(
+        label='Дата рождения',
+        widget=forms.DateInput(attrs={'type': 'date'})
+    ) 
+
+
+# IntegerField для целочисленных полей,
+# CharField для текстовых полей,
+# DateField для полей с датой.
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 
+from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
+
+
+def bbirthday(request):
+    # print(request.GET)  # Напечатаем. 
+    if request.GET:
+        form = BirthdayForm(request.GET)
+        if form.is_valid():
+            # считаем, сколько дней осталось до дня рождения.
+            pass
+    else:
+        form = BirthdayForm()
+    context = {'form': form}
+    # return render(request, 'birthday/birthday.html', context=context)
+    return render(request, 'birthday/birthday.html', context)
+
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    form = BirthdayForm(request.GET or None)
+    context = {'form': form}
+    if form.is_valid():
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
